9815866c587f43951318a27217b12b22fe121aab
julian-borray <julian.borray@mercadolibre.com.co>
2021-12-28 12:04:03 -0500

    add form-data

--- back/src/modules/chat/controllers/ChatController.ts
+++ back/src/modules/chat/controllers/ChatController.ts
@@ -1,3 +1,5 @@
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 import express = require('express');
 import {ChatProvider} from "../providers/ChatProvider";
 
@@ -29,6 +31,26 @@ export class ChatController {
                 res.status(500).json({message: err});
             }
         });
+        this.router.post('/message/upload', upload.array('doc', 5), (req, res)=>{
+            try{
+                if (req.files) {
+                    console.log(req.files);
+                    res.status(200).send(req.files);
+                } else {
+                    res.status(500).json({message: 'no hay archivos en el mensaje'});
+                }
+            } catch (err) {
+                res.status(500).json({message: err});
+            }
+        })
+        this.router.post('/message/base-upload',  (req, res)=>{
+            try{
+                console.log(req.body);
+                res.status(200).send(req.body);
+            } catch (err) {
+                res.status(500).json({message: err});
+            }
+        })
         return this.router;
     }
 }
